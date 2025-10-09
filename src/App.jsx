ac9c3381af4a2fa3405f4790e726cf9150158b7a
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-09 14:37:38 -0700

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Footer from "./components/Footer"
 import Content from "./components/Content"
 import data from "./data.js"
 
+
 export default function App() {
   const contentElements = data.map((content) => {
     return (
       <Content
         key={content.id}
         content={content}
-        // name={content.name}
-        // type={content.type}
-        // hp={content.hp}
-        // defense={content.defense}
-        // img={content.img}
+        name={content.name}
+        type={content.type}
+        hp={content.hp}
+        defense={content.defense}
+        img={content.img}
       />
     )
   })
